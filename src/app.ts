d06690d4e32fbdd727b8aeb1c215c112eb7e28e9
teamosishq <teamosishq@gmail.com>
2025-01-28 16:26:09 +0600

    update cors

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,10 @@ const corsProtectedOptions: cors.CorsOptions =
         optionsSuccessStatus: 204,
       }
     : {
-        origin: ["https://erp.teamosis.com", "tf-erp-solution.vercel.app"],
+        origin: [
+          "https://erp.teamosis.com",
+          "https://tf-erp-solution.vercel.app",
+        ],
         methods: "GET,PUT,PATCH,POST,DELETE",
         preflightContinue: false,
         optionsSuccessStatus: 204,
